f1045766c7eaa6755087f0142dbdbc5191aac08a
Veysel Kurhan <veysel.kurhan@argusmedya.com>
2016-11-04 16:08:01 +0300

    Schema support for Postgresql

--- DynamicLinqPadPostgreSqlDriver/SQL/QueryTables.sql
+++ DynamicLinqPadPostgreSqlDriver/SQL/QueryTables.sql
@@ -1,3 +1,6 @@
-﻿SELECT table_catalog AS "TableCatalog", table_name AS "TableName" 
+﻿SELECT 
+	table_catalog AS "TableCatalog", 
+	table_name  AS "TableName",
+	table_schema AS "TableSchema"
 FROM information_schema.tables 
-WHERE table_type='BASE TABLE' AND table_schema='public';
+WHERE table_type='BASE TABLE' AND table_schema in (select schema_name from information_schema.schemata where schema_owner != 'postgres' or schema_name='public')
