cb746600f9e80b434cf771559c6ab91948f53416
teejay-87 <tommasosala87@gmail.com>
2017-08-02 17:25:20 +0200

    Incremented version

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("0.2.1.0")]
-[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.2.1.1")]
+[assembly: AssemblyFileVersion("0.2.1.1")]
